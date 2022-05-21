9dbdf1fd2dc86266b78fb26243ae68527024edd8
zhuqiongqiao <1903343499@qq.com>
2022-05-21 22:10:25 +0800

    第一次提交

---
+++ backMusic.java
@@ -0,0 +1,32 @@
+package 学生信息管理系统;
+import javax.sound.sampled.*;
+import java.io.File;
+public class backMusic {
+    static void playMusic() {// 背景音乐播放
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(new File("C:\\Users\\朱雨晴\\Desktop\\黄霄雲-星辰大海.wav"));    //绝对路径
+            AudioFormat aif = ais.getFormat();
+            final SourceDataLine sdl;
+            DataLine.Info info = new DataLine.Info(SourceDataLine.class, aif);
+            sdl = (SourceDataLine) AudioSystem.getLine(info);
+            sdl.open(aif);
+            sdl.start();
+            FloatControl fc = (FloatControl) sdl.getControl(FloatControl.Type.MASTER_GAIN);
+            // value可以用来设置音量，从0-2.0
+            double value = 2;
+            float dB = (float) (Math.log(value == 0.0 ? 0.0001 : value) / Math.log(10.0) * 20.0);
+            fc.setValue(dB);
+            int nByte = 0;
+            final int SIZE = 1024 * 64;
+            byte[] buffer = new byte[SIZE];
+            while (nByte != -1) {
+                nByte = ais.read(buffer, 0, SIZE);
+                sdl.write(buffer, 0, nByte);
+            }
+            sdl.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
